ea089da443512688a69d4e9b7f725c2c368f4294
yudiarami11 <yudiarami00@gmail.com>
2025-07-10 00:35:56 +0700

    perubahan pada warna top bar
    mengubah warna top bar

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->theme(asset('css/filament/admin/theme.css'))
             ->brandLogoHeight('3rem')
             ->favicon(asset('images/Uin.png'))
             // ->brandLogo(asset('images/UIn2.png'))
